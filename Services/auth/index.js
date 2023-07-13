b632a6c1757d5c08880ab65af444d94a7e377a1a
ANAND RAJ <83631626+AnandRaj2020@users.noreply.github.com>
2023-07-13 12:55:39 +0530

    Add files via upload

---
+++ Services/auth/index.js
@@ -0,0 +1,36 @@
+
+export const register = async (formData) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        const data = res.json();
+        return data;
+    } catch (error) {
+        console.log('error in register (service) => ', error);
+    }
+}
+
+export const login = async (formData) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':  '*',
+                'ngrok-skip-browser-warning' : true,
+                'Origin': window.location.origin
+            },
+            body: JSON.stringify(formData),
+        })
+        const data = res.json();
+        return data;
+    } catch (error) {
+        console.log('error in login (service) => ', error);
+    }
+}
+
---
+++ pages/frontend/postAJob.jsx
@@ -0,0 +1,180 @@
+import NavBar from '@/components/NavBar'
+import Select from 'react-select'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { add_job } from '@/Services/job';
+import { useRouter } from 'next/router'
+
+
+export default function PostAJob() {
+    const user = useSelector(state => state.User.userData)
+    const router = useRouter();
+
+    const [formData, setFormData] = useState({ user: user?._id, title: "", compensation: 0, email: "", company: "", description: "", job_category: "", job_type: "", job_experience: "", job_vacancy: 0, application_cutoff_date: "" });
+    const [error, setError] = useState({ user: "", title: "", compensation: "", email: "", company: "", description: "", job_category: "", job_type: "", job_experience: "", job_vacancy: "", application_cutoff_date: "" });
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+
+
+        if (!formData.title) {
+            setError({ ...error, title: "title Field is required" })
+            return;
+        }
+
+        if (!formData.compensation) {
+            setError({ ...error, salary: "salary Field is required" })
+            return;
+        }
+
+        if (!formData.email) {
+            setError({ ...error, email: "Email Field is Required" })
+            return;
+        }
+
+
+        if (!formData.company) {
+            setError({ ...error, company: "company Field is required" })
+            return;
+        }
+        if (!formData.description) {
+            setError({ ...error, description: "description Field is required" })
+            return;
+        }
+        if (!formData.job_category) {
+            setError({ ...error, job_category: "job_category Field is required" })
+            return;
+        }
+        if (!formData.job_type) {
+            setError({ ...error, job_type: "job_type Field is required" })
+            return;
+        }
+        if (!formData.job_experience) {
+            setError({ ...error, job_experience: "job_experience Field is required" })
+            return;
+        }
+        if (!formData.job_vacancy) {
+            setError({ ...error, job_vacancy: "job_vacancy Field is required" })
+            return;
+        }
+        if (!formData.application_cutoff_date) {
+            setError({ ...error, application_cutoff_date: "application_cutoff_date Field is required" })
+            return;
+        }
+
+        if (formData.user == null) {
+            return toast.error("Please Login First");
+        }
+
+        const res = await add_job(formData);
+        if (res.success) {
+            toast.success(res.message);
+            setTimeout(() => {
+                router.push('/frontend/displayJobs')
+            }, 1000)
+        }
+        else {
+            toast.error(res.message);
+        }
+    }
+
+
+
+    const options = [
+        { value: 'fulltime', label: 'Full Time' },
+        { value: 'parttime', label: 'Part Time' },
+        { value: 'internship', label: 'Internship' },
+        { value: 'contract', label: 'Contract' },
+    ]
+
+
+
+
+
+
+    return (
+        <>
+            <NavBar />
+            <div className='w-full  py-20 flex items-center  justify-center flex-col'>
+                <h1 className='text-xl mt-4 uppercase tracking-widest border-b-2 border-b-red-600 py-2 font-semibold mb-8 md:text-2xl lg:text-4xl'>Enter Job Details</h1>
+                <form onSubmit={handleSubmit} className="sm:w-1/2 w-full px-4 mx-4  h-full" >
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="title" className='mb-1 text-base font-semibold'>Title :</label>
+                        <input onChange={(e) => setFormData({ ...formData, title: e.target.value })} type="text" id='title' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter title of job' />
+                        {
+                            error.title && <p className="text-sm text-red-500">{error.title}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="salary" className='mb-1 text-base font-semibold'>Salary :</label>
+                        <input onChange={(e) => setFormData({ ...formData, compensation: e.target.value })} type="number" id='salary' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Salary for this job' />
+                        {
+                            error.salary && <p className="text-sm text-red-500">{error.compensation}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="email" className='mb-1 text-base font-semibold'>Email :</label>
+                        <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} type="email" id='email' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Email to be Contacted for this job' />
+                        {
+                            error.email && <p className="text-sm text-red-500">{error.email}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="company" className='mb-1 text-base font-semibold'>Company :</label>
+                        <input onChange={(e) => setFormData({ ...formData, company: e.target.value })} type="text" id='company' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Company of job' />
+                        {
+                            error.company && <p className="text-sm text-red-500">{error.company}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="description" className='mb-1 text-base font-semibold'>Description :</label>
+                        <textarea onChange={(e) => setFormData({ ...formData, description: e.target.value })} onResize={"none"} type="text" id='description' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter description of job' />
+                        {
+                            error.description && <p className="text-sm text-red-500">{error.description}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="jobCategory" className='mb-1 text-base font-semibold'>Job Category :</label>
+                        <input onChange={(e) => setFormData({ ...formData, job_category: e.target.value })} type="text" id='jobCategory' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Category of job' />
+                        {
+                            error.job_category && <p className="text-sm text-red-500">{error.job_category}</p>
+                        }
+                    </div>
+                    <Select onChange={(e) => setFormData({ ...formData, job_type: e.value })} placeholder="Please Select Job type" options={options} />
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        {
+                            error.job_category && <p className="text-sm text-red-500">{error.job_category}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="jobExperience" className='mb-1 text-base font-semibold'>Job Experience :</label>
+                        <input onChange={(e) => setFormData({ ...formData, job_experience: e.target.value })} type="text" id='jobExperience' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Experience Required for this job' />
+                        {
+                            error.job_experience && <p className="text-sm text-red-500">{error.job_experience}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="jobva" className='mb-1 text-base font-semibold'>Job Vacancy :</label>
+                        <input onChange={(e) => setFormData({ ...formData, job_vacancy: e.target.value })} type="number" id='jobva' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Number  of Vacancies' />
+                        {
+                            error.job_vacancy && <p className="text-sm text-red-500">{error.job_vacancy}</p>
+                        }
+                    </div>
+                    <div className='w-full mb-4  flex flex-col items-start justify-center'>
+                        <label htmlFor="jobva" className='mb-1 text-base font-semibold'>Job Deadline :</label>
+                        <input onChange={(e) => setFormData({ ...formData, application_cutoff_date: e.target.value })} type="date" id='jobva' className='w-full py-2 px-3 mb-2 border border-red-600 rounded' placeholder='Enter Deadline of job' />
+                        {
+                            error.application_cutoff_date && <p className="text-sm text-red-500">{error.application_cutoff_date}</p>
+                        }
+                    </div>
+                    <button type="submit" className='w-full py-2 rounded bg-red-600 text-white font-semibold tracking-widest'>Submit</button>
+                </form>
+            </div>
+            <ToastContainer />
+        </>
+    )
+}
